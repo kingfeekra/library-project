29e8f5aa658b86e7a03e702a42a927144e6ec514
kingfeekra <fiachraking@gmail.com>
2022-07-13 19:56:09 +0100

    Add various functionality to library
    Add inputs and submit button in HTML file
    
    Submit button adds new book stored in an object in JS file
    
    books are displayed in a list
    
    Remove button added to remove books but no functionality yet

--- script.js
+++ script.js
@@ -20,11 +20,51 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-const book1 = new Book("The Hobbit", "JRR Tolkien", 256, "has been read");
+
 
 function addBookToLibrary() {
-    myLibrary.push(book1);
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#read").value;
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
     console.log(myLibrary);
+    console.log(title);
 }
 
-addBookToLibrary();
+const list = document.querySelector("#list");
+const books = [];
+
+function displayBooks() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        books[i] = document.createElement("li");
+        books[i].setAttribute("data-index-number", i);
+        books[i].textContent = myLibrary[i].title;
+        list.appendChild(books[i]);
+    }
+}
+
+function addButton() {
+    const listItems = document.querySelectorAll("li");
+    const buttons = [];
+    for(let i = 0; i < listItems.length; i++) {
+        buttons[i] = document.createElement("button");
+        buttons[i].textContent = "Remove";
+        buttons[i].classList.add("removeButton")
+        listItems[i].appendChild(buttons[i]);
+        }
+}
+
+const submitButton = document.querySelector("#submitButton");
+submitButton.addEventListener("click", () => {
+    const listItems = document.querySelectorAll("li");
+    for(let i = 0; i < listItems.length; i++) {
+    list.removeChild(listItems[i]);
+    }
+    addBookToLibrary();
+    displayBooks();
+    addButton();
+});
+
+
